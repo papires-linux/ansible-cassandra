d001117e14c7f0055220145a9b0a682e68485c53
Your Name <you@example.com>
2021-04-06 11:50:56 -0300

    Projeto original

---
+++ roles/cassandra-cluster/templates/tables.sql
@@ -0,0 +1,13 @@
+CREATE KEYSPACE IF NOT EXISTS simulacao WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 3 };
+
+
+ALTER KEYSPACE "simulacao" WITH REPLICATION =
+  { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
+
+
+CREATE TABLE simulacao.modelo_table(
+   id UUID PRIMARY KEY, 
+   coluna01 text, 
+   coluna02 text 
+);
+
